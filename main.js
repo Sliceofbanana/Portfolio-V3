98fa7296c45a2e98752b6a076b0e9eebf84aa89e
Genesis Jr <72869897+Sliceofbanana@users.noreply.github.com>
2025-03-07 12:27:01 +0800

    030725 - with domain na yaz

--- main.js
+++ main.js
@@ -268,3 +268,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-DQPK1T2H31');
